f5e6d894c31640298f136b4877ca0ca2e77594d9
Timothy Engqvist Johansson <timothy@blockfirm.se>
2019-10-04 01:23:02 +0200

    Gracefully shut down servers

--- src/index.js
+++ src/index.js
@@ -4,3 +4,23 @@ import Proxy from './Proxy';
 const proxy = new Proxy(config);
 
 proxy.start();
+
+const onExit = () => {
+  console.log('Gracefully shutting down...');
+
+  proxy.stop()
+    .then(() => {
+      console.log('Graceful shutdown completed');
+      return 0;
+    })
+    .catch(error => {
+      console.log('Graceful shutdown failed with error:', error.message);
+      return 1;
+    })
+    .then(code => {
+      process.kill(code);
+    });
+};
+
+process.stdin.resume();
+process.on('SIGINT', onExit);
--- test/mocks/client/index.js
+++ test/mocks/client/index.js
@@ -7,4 +7,14 @@ client.connect().catch(error => {
   console.error('[MOCK] Unable to connect to Pine Lightning:', error.message);
 });
 
+const onExit = () => {
+  console.log('Gracefully shutting down...');
+  client.disconnect();
+  console.log('Graceful shutdown completed');
+  process.kill(0);
+};
+
+process.stdin.resume();
+process.on('SIGINT', onExit);
+
 export default client;
