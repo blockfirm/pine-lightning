18c9e6eb2bf900ce3a428343ef35fbf58850b5af
Timothy Engqvist Johansson <timothy@blockfirm.se>
2020-02-24 00:12:32 +0100

    Return an empty object from closeChannel rpc method

--- src/methods/closeChannel.js
+++ src/methods/closeChannel.js
@@ -9,24 +9,28 @@ const closeChannel = ({ lnd }) => {
     return Promise.reject(new Error('Missing lnd node'));
   }
 
-  return lnd.lnrpc.listChannels({}).then(({ channels }) => {
-    const gatewayChannel = channels && channels.find(channel => {
-      return channel.remote_pubkey === config.lnd.gateway.publicKey;
-    });
+  return lnd.lnrpc.listChannels({})
+    .then(({ channels }) => {
+      const gatewayChannel = channels && channels.find(channel => {
+        return channel.remote_pubkey === config.lnd.gateway.publicKey;
+      });
 
-    if (!gatewayChannel) {
-      return Promise.reject(new Error('No open channels found'));
-    }
-
-    const [txid, outputIndex] = gatewayChannel.channel_point.split(':');
-
-    return lnd.lnrpc.closeChannel({
-      channel_point: {
-        funding_txid_str: txid,
-        output_index: Number(outputIndex)
+      if (!gatewayChannel) {
+        return Promise.reject(new Error('No open channels found'));
       }
+
+      const [txid, outputIndex] = gatewayChannel.channel_point.split(':');
+
+      return lnd.lnrpc.closeChannel({
+        channel_point: {
+          funding_txid_str: txid,
+          output_index: Number(outputIndex)
+        }
+      });
+    })
+    .then(() => {
+      return {};
     });
-  });
 };
 
 export default closeChannel;
